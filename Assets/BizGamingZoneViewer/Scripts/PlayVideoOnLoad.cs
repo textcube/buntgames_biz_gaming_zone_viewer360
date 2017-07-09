8d05781bb0012f76d33c2e9f67ad72989a824722
hompy <hompy@buntgames.com>
2017-07-10 08:40:00 +0900

    init project

---
+++ Assets/BizGamingZoneViewer/Scripts/PlayVideoOnLoad.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideoOnLoad : MonoBehaviour {
+    public MovieTexture _movie;
+    private AudioSource _audio;
+    public AudioClip _clip;
+
+    void Start () {
+        _audio = gameObject.AddComponent<AudioSource>();
+        _audio.clip = _clip;
+        _audio.loop = true;
+        _movie.loop = true;
+        _audio.Play();
+        _movie.Play();
+	}
+}
